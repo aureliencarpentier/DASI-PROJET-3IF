b8aa7d1959c13bb7e9eba25803a4bf78085d4894
Sweetmimike <aurecarp@live.fr>
2020-04-10 15:39:54 +0200

    Creation de profilDao

---
+++ src/main/java/fr/insalyon/dasi/dao/ProfilAstralDao.java
@@ -0,0 +1,26 @@
+package fr.insalyon.dasi.dao;
+
+import fr.insalyon.dasi.metier.modele.Client;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import fr.insalyon.dasi.metier.modele.ProfilAstral;
+
+/**
+ *
+ * @author DASI Team
+ */
+public class ProfilAstralDao {
+    
+    public void creer(ProfilAstral profil) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.persist(profil);
+    }
+    
+    public ProfilAstral chercherParId(Long profilId) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        return em.find(ProfilAstral.class, profilId); 
+    }
+    
+    // modifier / supprimer  ... 
+}
